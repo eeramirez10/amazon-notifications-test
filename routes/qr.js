0cce945a729a9a5a3a4462644ca0abadb698f63a
Erick <eeramirez@tuvansa.com.mx>
2022-08-05 18:11:13 -0500

    Vcard

--- routes/qr.js
+++ routes/qr.js
@@ -4,7 +4,23 @@ const router = require("express").Router();
 
 router.get('/', (req, res)=>{
 
-    res.send(' hola')
+    var vCardsJS = require('vcards-js');
+
+    vCard = vCardsJS();
+ 
+    //set properties
+    vCard.firstName = 'Eric';
+    vCard.middleName = 'J';
+    vCard.lastName = 'Nesser';
+    vCard.organization = 'ACME Corporation';
+ 
+    //set content-type and disposition including desired filename
+    res.set('Content-Type', 'text/vcard; name="enesser.vcf"');
+    res.set('Content-Disposition', 'inline; filename="enesser.vcf"');
+ 
+    //send the response
+    res.send(vCard.getFormattedString());
+
 })
 
 
